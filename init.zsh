bff432cfc647992c6b79cef600a546dccab00156
JamesWP <james_peach01@hotmail.co.uk>
2015-12-17 01:09:07 +0000

    added new PS1 and configured tabs and menu[C

--- init.zsh
+++ init.zsh
@@ -9,13 +9,15 @@ export PATH=$PATH:/Users/james/installs/mongodb/bin
 export PATH=$PATH:/Users/james/bin
 export PATH=$PATH:.
 
-
-
-# The following lines were added by compinstall
 zstyle :compinstall filename '~/.zsh/init'
 
 autoload -Uz compinit
 compinit
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*' list-colors 'di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
+
 # End of lines added by compinstall
 # Lines configured by zsh-newuser-install
 HISTFILE=~/.histfile
@@ -24,14 +26,26 @@ SAVEHIST=1000
 # End of lines configured by zsh-newuser-install
 eval $(thefuck --alias)
 
-# correction
-setopt correctall
 
 # prompt
-autoload -U promptinit
-promptinit
-      
-source ~/.zsh/functions/prompt_jair_setup
+setopt PROMPT_SUBST
+autoload -U colors && colors
+
+# Zsh to use the same colors as ls
+PROMPT="%{$fg[blue]%}%m %{$fg[yellow]%}$(python ~/.zsh/scripts/path.py) %{$reset_color%}%{$fg[green]>%} %{$reset_color%}"
+
+# show dir when first tab pressed
+first-tab() {
+    if [[ $#BUFFER == 0 ]]; then
+        BUFFER="cd "
+        CURSOR=3
+        zle list-choices
+    else
+        zle expand-or-complete
+    fi
+}
+zle -N first-tab
+bindkey '^I' first-tab
 
 
 # standard config
@@ -46,7 +60,7 @@ alias tmux="tmux -2"
 alias vi='mvim -v'
 alias vim='mvim -v'
 alias :wq='exit'
-alias ll='ls -lh'
+alias ll='ls -lGh'
 alias serve=nws
 alias gs='git status'
 alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip'
